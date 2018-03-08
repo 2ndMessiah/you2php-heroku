77efc6e3bde7f5c82785a9dcab36e98e9ecc4e4b
2ndMessiah <lym.0072003@gmail.com>
2018-03-08 17:31:11 +0800

    Delete config.php

--- web/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyC04E1eM_XOq12Dz07zSbV7mTniVSkx1Q8');//这里改成您的api key
-define('GJ_CODE', 'US');//国家代码
-define('SITE_NAME', 'drink');//网站名字
-define('TITLENAME', 'iceberg');//网站名字
-define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', 'lym.0072003@gmail.com');//邮箱
-?>
